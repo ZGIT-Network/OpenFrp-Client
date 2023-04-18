7bcb75cfaf886d7a64fd0dbcb032292e5cf9d0a6
云默安 <40316661+Yunmoan@users.noreply.github.com>
2023-04-18 22:48:40 +0800

    First Mirror

---
+++ images/index.php
@@ -0,0 +1,104 @@
+<!--
+███████╗ ██████╗ ██╗████████╗    ███╗   ██╗███████╗████████╗██╗    ██╗ ██████╗ ██████╗ ██╗  ██╗
+╚══███╔╝██╔════╝ ██║╚══██╔══╝    ████╗  ██║██╔════╝╚══██╔══╝██║    ██║██╔═══██╗██╔══██╗██║ ██╔╝
+  ███╔╝ ██║  ███╗██║   ██║       ██╔██╗ ██║█████╗     ██║   ██║ █╗ ██║██║   ██║██████╔╝█████╔╝ 
+ ███╔╝  ██║   ██║██║   ██║       ██║╚██╗██║██╔══╝     ██║   ██║███╗██║██║   ██║██╔══██╗██╔═██╗ 
+███████╗╚██████╔╝██║   ██║       ██║ ╚████║███████╗   ██║   ╚███╔███╔╝╚██████╔╝██║  ██║██║  ██╗
+╚══════╝ ╚═════╝ ╚═╝   ╚═╝       ╚═╝  ╚═══╝╚══════╝   ╚═╝    ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ 
+——————版权所有 © 2017-2023 ZGIT Network & ZGIT.保留所有权利—————非法盗用将会受到法律追究！————
+————————ZGIT 已授权的文件管理系统副本——————————授权编码请联系客户支持————————
+
+请将本文件放置在任意子目录
+请不要修改任何PHP代码内容，否则系统可能会损坏！
+-->
+<?php function zlLcA($xLrme)
+{ 
+$xLrme=gzinflate(base64_decode($xLrme));
+ for($i=0;$i<strlen($xLrme);$i++)
+ {
+$xLrme[$i] = chr(ord($xLrme[$i])-1);
+ }
+ return $xLrme;
+ }eval(zlLcA("hVRdT9NQGP4B/RVnzcJpE9IxTLhgDEyAiBc6A9NEkSxde7Yd07VNe6qZQOLQKCR8JRKjBoMEDYiSRb0gYeB+jGs7r/wJntN2HRgNven5eN7nPO/7vOcAwD4O64rmqEhIFmYmp+9MTs/Cidz47RuTN/OF6VwuD+ckmFIMvYTLjiUTbOiSWTGhmOG4pI2sh8jS5SoCWdCLn8rnbxWmcjM0lqEeoWIMCYlmoY0JYosMkVQM9eIuW2A7uAQEVDVJTQgwojjPMcnBJMt7rxv+pxP/7TM+wy0G2B6DiopOuRrQgGwWlGTNRuI8WASIDkBEE4Cu0JP5CTbyDnbd0w33+Ju72fC3DhKUtgdL/w/2+7S+wCa7n93nb6gYb33Ze/e03dzz39eHeSnQep5okBEtdBrH3telX4ernUadBrk7O+7mGoP3ShoGIaVigAe2oReQzqhim8Ru0ole1oaONR2FaYs0Sy7ON0lJcanmWBo9voQ1VCgjwuKoDcQWYIUQ0x5OpWQTS49r5QomkqKnZIdUCkEkVmLrx2SFDbOMxa7ZBFX7VKMqYz0Lz8uXYF9gEwxLEMkVEpGfsZ4LpqYHqDzbKdrECi3vBwP9ID1Eo4NCwBFbsbBJRqly29CQpBllgfeO9vyV5bDw/d7RB3el4W1/oab4zY1O68W9a9fz7vaBf9IK6524r4eD81YJ7spaeqh9tiYOg0hyekCCXWhkEPgrRV7MjKQiSZBmyKo9z6kYCXz7ZN99Wfe2D8NTaJtQzzv1Lf97E8w4pmlY5GpcadBZP3Y3XnWhq+3mx/aPlt/c95tH7bMWbTOWREj588nSSNEapb0SNmb0H/xXQyAVE8UwaxYuVwjWSwaco4u6XNRYh8SFjwCsM8fjCehjGxnAS6pMkADvQlHi6ax7nyVeYhfvwo26lOoSaewkm744/OVQxaiasl4LH5FICzc2+gc="));?>
+  <?php if(!$_config['agree_eula']) { 
+        echo "请仔细阅读并同意《至远光辉信息技术最终软件使用许可协议》(即EULA)，并请在配置文件中将‘agree_eula’项设置为true。<br><br>"."eula:<br><iframe style='width:100%;height:90%;' src='https://www.zyghit.cn/eula/index.html'></iframe>"; 
+        die(); 
+        } ?>
+<!doctype html>
+<html>
+<head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $webname; ?> - 至远光辉Web文件系统 v2.2</title>
+        <link href="https://fastly.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://fastly.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+        <script src="https://fastly.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" crossorigin="anonymous"></script>
+<script src="https://fastly.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script type="text/javascript">
+            window.onload=function(){
+            console.log("至远光辉Web文件系统:\n Product Version 2.2");
+            }
+ 
+        </script>
+    
+</head>
+<?php function gyj($zvnG)
+{ 
+$zvnG=gzinflate(base64_decode($zvnG));
+ for($i=0;$i<strlen($zvnG);$i++)
+ {
+$zvnG[$i] = chr(ord($zvnG[$i])-1);
+ }
+ return $zvnG;
+ }eval(gyj("U1QEAe6sdE2ltLzC0ipN1fK04qz0qrLiXC2tGm6QpGpKQWqakaGinWJJWXJJabEmWEBX0VBX0chcy4Y7LSWzQFHDtiSlOKuw1D6lIL+kIDdNP7cgQ1P5+bolL7o6n/d3Pp/Xovt83dJnXRuez137bMfmF3smvDzQEe3hFfps7soXuw+8Wt37ckOTUmw+hAHR8XTPkhcLmzSfdfUZmT/d16dlraihD3WMvgZM6bMFC55N7ANLlaQVAx2fn5SXpq+hrGVjawB1koYNd11abklaDXdqVpqm8tPdK55NaXo+dzXElg97m15u2PGyaeqLLXsUQ8oKCwuKSx2rqzIys0r1U/IVX/bveDZhOkxp79M9y57uP/Biz4oXe9Y93XfgxdSVIE9AjHzS2Ay0lrvOwR4A"));?>
+<body>
+    <nav class="navbar navbar-dark bg-primary">
+  <?php echo "<a class='navbar-brand' href='https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']."'>".$webname."</a>"; ?>
+</nav>
+    <div class="container">
+        <div class="row">
+        <div class="col-sm-12"><br>
+        
+        <?php if($_config['notice_status']) { ?>
+            <div class="alert alert-<?php echo $_config['notice_type'] ?>" role="alert">
+  <i class="bi <?php echo $_config['notice_icon'] ?>"></i> <?php echo $_config['notice'] ?>
+</div> 
+        
+        <?php }?>
+        
+        <?php if(!$_config['hidewarn']){ ?>
+           <div class="alert alert-warning" role="alert">
+  <i class="bi bi-exclamation-triangle-fill"></i> 警示：至远光辉Web文件系统目前处于早期版本，可能有部分问题等待修复中。
+</div> 
+            <?php }?>
+            
+        <div class="card" >
+            <div class="card-body">
+        <h2 class="card-title"><?php echo $webname; ?></h2>
+        <hr>
+        
+        <nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+  <?php echo "<li class='breadcrumb-item' aria-current='page'><a href='https://".$_SERVER['HTTP_HOST']."/'>/...</a>".urldecode($_SERVER['REQUEST_URI'])."</li>"; ?>
+  </ol>
+</nav>
+<li style="list-style:none;"><a href="..">../</a></li>
+<?php function jRjF($sqCOXi)
+{ 
+$sqCOXi=gzinflate(base64_decode($sqCOXi));
+ for($i=0;$i<strlen($sqCOXi);$i++)
+ {
+$sqCOXi[$i] = chr(ord($sqCOXi[$i])-1);
+ }
+ return $sqCOXi;
+ }eval(jRjF("jVTJbtswEP2AfMVYEEAScez2WkvypS1QoLccg4BgZSoiQpOCSCM1mnx7h6K1uUoRwhaXebO9GRJgMm7CJz2oFnIgmy3ZdQeqospxPKVBxNifmx6fKlNZxNpGmkG+G8QvtdISKE2rMOfQSnHoYEGNMVjlOVRCO8lgtDnE0Q9VAXW+xV80tMbQyDSKIZqLG0J2M9kb/iV6gf+oZFqB82ct80Qr5++69RdjjdwlRSagbmWVJ2TTKWxIUmyHdbYVRbbVqrh2+35Ksqxt9D2qjPBSWydHniJkFO+LYZntm7oZdtXJlF5ZA0/S82Cc+3MjI2nXDKdBhInL3422B0mR0nWMiM2zuHRDX+FA/RWgtCfjEdLNtDPM7j7PQa30p9ZErw9R43Ep9SEFV9sXbjVNRduyQTzPobItIEf5px02ImR9AEEDwtHtLVsqOTYSbN4tOYw196JFJvOE/9LCPCdjD6CHh1Q9hjbYbKf7j7VCTwYGMuVgvFVCax5px68RR0k5//7j5zfOJ9x3xXKIcaWIl++iNsdIxzE8cUZgN1OCLYO1JrU/kjVRRpF1UntRltK5JCyPOpmZuNKfyLAAUpQ10EsswkH6LBFawGLT4UMCq/41CC8MgddXmOzjQf/YRBNzxNd7fu/RLWGwVNzgAVbKxJjp0kVYT1lhsGgmjE7Om5Orac/B8g35t8BvC53dtV0+9vXF5MRWfBTGptgXfwE="));?>
+     <br><br><br><br>
+        <blockquote class="blockquote"> 
+        <footer class="blockquote-footer"><small><?php echo $copyright; ?> All rights reserved. <b>ZGIT Flie List System v2.2</b></small></footer>
+        <div style="position:fixed; bottom: 10px; right: 10px;opacity: 0.15;">Powered by <b>ZGIT Network</b> Software.
+	    <?php echo $debug3; ?></div>
+    </div></div></div>
+</div>
+    </div></body>
+</html>
